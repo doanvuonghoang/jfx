01f5cd0a5c097353a27e188d7bc632b0c5b6389f
Hoàng <Hong@Terry-LAP.mshome.net>
2012-12-11 15:56:45 +0700

    changes

---
+++ exts/jf/auth/ext.cnf.php
@@ -0,0 +1,16 @@
+<?php
+
+$config = array(
+	'srcs'		=> array(
+		'AuthDP.php',
+		'install.sql',
+		'uninstall.sql'
+	),
+	'install_scripts'	=> array(
+		'install.sql'
+	),
+	'uninstall_scripts'	=> array(
+		'uninstall.sql'
+	)
+);
+?>
