9a400cc7aa23d7333d4aa37406ff0e9a417bb480
Hoàng <Hong@Terry-LAP.mshome.net>
2012-12-12 10:47:55 +0700

    changes

---
+++ exts/jf/session/ext.def.php
@@ -0,0 +1,16 @@
+<?php
+
+$config = array(
+	'srcs'		=> array(
+		'MySQLSessionDP.php',
+		'install.sql',
+		'uninstall.sql'
+	),
+	'install_scripts'	=> array(
+		'install.sql'
+	),
+	'uninstall_scripts'	=> array(
+		'uninstall.sql'
+	)
+);
+?>
