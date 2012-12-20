f940dffa4a7bd47c69bce0ebe26b944a40481ed2
Hoàng <Hong@Terry-LAP.mshome.net>
2012-12-20 09:35:23 +0700

    Begin starting an app

---
+++ lib/jf/app/JFApp.php
@@ -0,0 +1,56 @@
+<?php
+namespace lib\jf\app;
+
+/**
+ * @author Hoàng
+ */
+class JFApp {
+	const MODE_VIEW = 'VIEW';
+	const MODE_EDIT = 'EDIT';
+	
+	private $cfgFile;
+
+	/**
+	 * @var \lib\jf\IConfiguration
+	 */
+	protected $cfg;
+	
+	/**
+	 * @var IAppContext
+	 */
+	protected $ac;
+	
+	private $locale;
+	
+	private $theme;
+	
+	private $mode;
+	
+	private $responseType;
+	
+	private $bundles;
+	
+	function __construct($cfgFile, IAppContext $ac) {
+		$this->cfgFile = $cfgFile;
+		$this->cfg = \lib\jf\core\ArrayConfiguration::loadFromFile($this->cfgFile);
+		$this->ac = $ac;
+		
+		$this->locale = $ac->getSession('locale');
+		if(!$this->locale) $this->cfg->getValue ('locale');
+		
+		$this->theme = $ac->getSession('theme');
+		if(!$this->theme) $this->cfg->getValue ('theme');
+		
+		$this->mode = $ac->getSession('mode');
+		if(!$this->mode) $this->mode = self::MODE_VIEW;
+		
+		$this->responseType = $_SERVER['HTTP_ACCEPT'];
+	}
+	
+	function start() {
+		echo '<pre>' . print_r($this->responseType, true) . '</pre>';
+		echo "<img src='{$this->ac->createRelativeResourceURL('/images/404.jpg')}' />";
+	}
+}
+
+?>
