fe8d430ff9dc2ec153441bc6891fb09889090324
Hoàng <Hong@Terry-LAP.mshome.net>
2012-12-20 14:17:23 +0700

    Begin starting an app

--- lib/jf/app/JFApp.php
+++ lib/jf/app/JFApp.php
@@ -5,9 +5,6 @@ namespace lib\jf\app;
  * @author Hoàng
  */
 class JFApp {
-	const MODE_VIEW = 'VIEW';
-	const MODE_EDIT = 'EDIT';
-	
 	private $cfgFile;
 
 	/**
@@ -20,35 +17,13 @@ class JFApp {
 	 */
 	protected $ac;
 	
-	private $locale;
-	
-	private $theme;
-	
-	private $mode;
-	
-	private $responseType;
-	
-	private $bundles;
-	
 	function __construct($cfgFile, IAppContext $ac) {
 		$this->cfgFile = $cfgFile;
 		$this->cfg = \lib\jf\core\ArrayConfiguration::loadFromFile($this->cfgFile);
 		$this->ac = $ac;
-		
-		$this->locale = $ac->getSession('locale');
-		if(!$this->locale) $this->cfg->getValue ('locale');
-		
-		$this->theme = $ac->getSession('theme');
-		if(!$this->theme) $this->cfg->getValue ('theme');
-		
-		$this->mode = $ac->getSession('mode');
-		if(!$this->mode) $this->mode = self::MODE_VIEW;
-		
-		$this->responseType = $_SERVER['HTTP_ACCEPT'];
 	}
 	
 	function start() {
-		echo '<pre>' . print_r($this->responseType, true) . '</pre>';
 		echo "<img src='{$this->ac->createRelativeResourceURL('/images/404.jpg')}' />";
 	}
 }
